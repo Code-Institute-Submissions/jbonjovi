366fe0f767d8f10244a7f0cebd7177ee10124b8a
JihanePett <jihane.petticrew@gmail.com>
2021-01-22 14:56:09 +0000

    tested accounts login as a superuser and setup allauth

--- jbonjovi/settings.py
+++ jbonjovi/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +70,20 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = 'accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+SITE_ID = 1
 
 WSGI_APPLICATION = 'jbonjovi.wsgi.application'
 
