546fe2623c1c2f4ea721155f29ee2aaf0078cb8e
JihanePett <jihane.petticrew@gmail.com>
2021-01-22 17:17:42 +0000

    added shopping bag app urls and template updated the link to the bag on the base and mobile templates

--- jbonjovi/settings.py
+++ jbonjovi/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -146,3 +147,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
