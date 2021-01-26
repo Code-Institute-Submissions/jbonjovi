8d5861429567bd99b494126313bd04379d51df70
JihanePett <jihane.petticrew@gmail.com>
2021-01-26 12:21:08 +0000

    updated the add, edit and remove view with a superuser only restriction

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -7,6 +8,7 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -21,7 +23,6 @@ def profile(request):
                            'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
-
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
